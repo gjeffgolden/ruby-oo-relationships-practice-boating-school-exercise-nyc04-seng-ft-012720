281f461867e4eae32e150dccbf91d62e02bac061
gjeffgolden <gjgolden08@gmail.com>
2020-12-28 13:11:25 -0700

    Tests passing, needs refactor.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,55 @@
 class Instructor
+    attr_accessor :name
+    
+    @@all = []
+
+    def initialize name 
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def all_tests
+        BoatingTest.all.select do |tests|
+            tests.instructor == self
+        end
+    end
+
+    def all_students 
+        all_tests.map {|test| test.student}
+    end
+
+    def passed_students 
+        all_tests.select do |test| 
+            if test.test_status == "passed"
+                return test.student
+            end
+        end
+    end
+
+    def pass_student student, test_name
+        BoatingTest.all.map do |tests| 
+            if tests.student == student && tests.test_name = test_name
+                tests.test_status = "passed"
+                return tests
+            else
+                BoatingTest.new student, test_name, "passed", self
+            end
+        end
+    end
+
+    def fail_student student, test_name 
+        BoatingTest.all.map do |tests|
+            if tests.student == student && tests.test_name = test_name
+                tests.test_status = "failed"
+                return tests
+            else 
+                BoatingTest.new student, test_name, "failed", self
+            end
+        end
+    end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,37 @@
 class Student
+    attr_accessor :first_name
+    
+    @@all = []
+
+    def initialize first_name
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def all_tests 
+        BoatingTest.all.filter {|boating_tests| boating_tests.student == self}
+    end
+
+    def add_boating_test test_name, test_status, instructor
+        BoatingTest.new self, test_name, test_status, instructor
+    end
+
+    def all_instructors
+        all_tests.map {|boating_test| boating_test.instructor}.uniq
+    end
+
+    def self.find_student first_name
+        all.find {|students| students.first_name == first_name}
+    end
+
+    def grade_percentage
+        passed_tests = all_tests.filter {|test| test.test_status == "passed"}
+        (passed_tests.count.to_f / all_tests.count.to_f) * 100
+    end
+
 
 end
